073ae4b6878ab7b65655f361219248ed985b8bca
gahnsei <gahnsei@gmail.com>
2022-02-24 15:06:19 -0600

    deplyment

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const ctrl = require(`./controller.js`)
+const ctrl = require(`./controller.js`);
 const app = express();
-
+const path = require(`path`);
 
 app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+app.get(`/`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../client/index.html`))
+})
+
 app.get("/api/compliment", ctrl.getCompliment);
-app.get(`/api/fortune`, ctrl.getAllFortune)
+app.get(`/api/fortune`, ctrl.getAllFortune);
 app.get(`/api/fortune/random`, ctrl.getFortune);
 app.post(`/api/fortune`, ctrl.addFortune);
 app.put(`/api/fortune`, ctrl.addFortune);
-app.delete(`/api/fortune/:user`, ctrl.deleteUser)
+app.delete(`/api/fortune/:user`, ctrl.deleteUser);
 
-app.listen(4000, () => console.log("Server running on 4000"));
+let port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`Have you boys seen my Goyard port on ${port}`));
